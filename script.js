6c63f914de6612d80449912a4712cfb59b7af5bd
HARIHARAN <hariharanys08jl2000@gmail.com>
2023-02-12 19:32:19 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,31 @@
+const hourEl = document.getElementById("hour")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+const ampmEl = document.getElementById("ampm")
+
+
+function updateclock(){
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+    let seconds = new Date().getSeconds()
+
+    let ampm = "AM"
+
+    if(hours > 12){
+        hours = hours - 12
+        ampm = "PM"
+    }
+    hours = hours < 10 ? "0" + hours : hours; 
+    minutes = minutes < 10 ? "0" + minutes : minutes; 
+    seconds = seconds < 10 ? "0" + seconds : seconds; 
+    
+    hourEl.innerText = hours
+    minutesEl.innerText = minutes
+    secondsEl.innerText = seconds
+    ampmEl.innerText = ampm
+
+    setTimeout(() => {
+        updateclock()
+    },1000)
+}
+updateclock()
